4f0b6100ad39f3acfdfe767ec64ac9adde22c9bb
Jasur <abduxakimovjasurbek7@gmail.com>
2025-06-29 19:27:24 +0500

    Pass all the info into time-track page Task styles in time-track page fix

--- time-track.js
+++ time-track.js
@@ -53,3 +53,41 @@ if (hoursE && minutesE && secondsE && toggleBtn) {
 
     updateDisplay();
 }
+
+
+// Take all the info
+
+
+let urlParams = new URLSearchParams(window.location.search);
+chosenGroupId = urlParams.get("groupId");
+groups = JSON.parse(localStorage.getItem("groups") || "[]");
+chosenGroup = groups.find(g => g.id === chosenGroupId);
+
+time_tasks_content = document.querySelector(".time-tasks-content");
+
+// Function to add tasks into content
+function addTask(task) {
+    time_tasks_new_task = `
+    <li class="time-tasks_item">
+        <label class="time-tasks_item-label common-checkbox-label">
+            <input type="checkbox" class="time-tasks_item-checkbox common-checkbox">
+            <span class="time-tasks_item-check common-checkbox-check"></span>
+            <span class="time-tasks_item-text common-checkbox-text">${task}</span>
+        </label>
+    </li>
+    `
+    let temp = document.createElement("div");
+    temp.innerHTML = time_tasks_new_task.trim();
+    let newTask = temp.firstElementChild;
+    time_tasks_content.appendChild(newTask);
+}
+
+// Taking all the tasks
+chosenGroupSubgroupsCount = chosenGroup.subgroups.length;
+for(let i = 0; i < chosenGroupSubgroupsCount; i++) {
+    chosenGroupSubgroupTaskCount = chosenGroup.subgroups[i].tasks.length;
+    for(let j = 0; j < chosenGroupSubgroupTaskCount; j++) {
+        chosenGroupSubgroupTask = chosenGroup.subgroups[i].tasks[j];
+        addTask(chosenGroupSubgroupTask);
+    }
+}
